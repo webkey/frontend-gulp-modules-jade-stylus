ecdeaaa36fa2910a7988428dc44e92265e9f1347
webkey <serhiy_i@ukr.net>
2020-02-27 22:52:23 +0300

    Create

---
+++ tasks/default.js
@@ -0,0 +1,38 @@
+import runSequence from 'run-sequence';
+import gulp from 'gulp';
+
+gulp.task('styles:dependencies', () => (
+	runSequence(
+		'sprites',
+		'icons',
+		'styles'
+	)
+));
+
+gulp.task('default', () => (
+	runSequence(
+		[
+			'libsCss',
+			'libsJs',
+			'scripts',
+			'styles:dependencies',
+			'images',
+			'templates'
+		],
+		'server',
+		'watch'
+	)
+));
+
+gulp.task('build', () => (
+	runSequence(
+		'libsCss',
+		'libsJs',
+		'styles:dependencies',
+		'imagesBuild',
+		'scripts:lint',
+		'scripts',
+		'copy',
+		'templates'
+	)
+));
---
+++ tasks/watch.js
@@ -0,0 +1,24 @@
+import gulp from 'gulp';
+import runSequence from 'run-sequence';
+import {get as browserSync} from 'browser-sync';
+import watch from 'gulp-watch';
+
+const bs = browserSync('server');
+
+gulp.task('watch', () => {
+	global.watch = true;
+
+	watch(['app/libs/css/*.js', 'libs-links.js'], () => runSequence('libsCss', bs.reload));
+	watch(['app/libs/js/*.js', 'libs-links.js'], () => runSequence('libsJs', bs.reload));
+	watch(['app/sprites/**/*.png', '!app/sprites/*.png'], () => runSequence('sprites'));
+	watch('app/{styles,blocks}/**/*.styl', () => {
+		runSequence(['styles', 'styles:lint'], () => bs.reload('assets/styles/app.min.css'));
+	});
+	watch(['app/blocks/**/*.js', 'app/scripts/**/**.js'], () => {
+		runSequence(['scripts'], () => bs.reload('assets/scripts/app.min.js'));
+	});
+	watch(['app/{pages,blocks}/**/*.jade'], () => runSequence('templates', bs.reload));
+	watch('app/resources/**/*', () => runSequence('copy', bs.reload));
+	watch('app/icons/**/*.svg', () => runSequence('icons', bs.reload));
+	watch(['app/images/**/*'], () => runSequence('images', bs.reload));
+});
